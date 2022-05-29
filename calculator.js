a6d49c07e93d2ed47cfd593f7449dc32388e8155
mattwg <mattwglowacki@gmail.com>
2022-05-29 17:06:28 -0400

    Add Operation Functions

--- calculator.js
+++ calculator.js
@@ -1,10 +1,36 @@
 const numBtn = document.querySelectorAll(".num");
-let currentNum = 0;
+const del = document.querySelector('.back');
+let currentNum = null;
+let eval = false;
 
 function setCurrent() {
+    if (currentNum) {
+        
+    }
     currentNum = this.textContent;
     console.log(currentNum);
 };
 
+del.addEventListener('click', () => currentNum = 0);
 numBtn.forEach(item => item.addEventListener('click', setCurrent));
 console.log(numBtn);
+
+function add(a,b) {
+    const sum = a + b;
+    return sum;
+}
+
+function diff(a,b) {
+    const diff = a - b;
+    return diff;
+}
+
+function mult(a,b) {
+    const prod = a * b;
+    return prod;
+}
+
+function div(a,b) {
+    const quot = a / b;
+    return quot;
+}
